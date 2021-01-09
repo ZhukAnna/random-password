04e14f48733361dfdeb4a64d1d48920cf67bc877
Anna Zhukova <anna.zhukanna@gmail.com>
2021-01-09 15:31:27 +0500

    script raw version

--- script.js
+++ script.js
@@ -1,16 +1,55 @@
 'use strict';
 
-const allsymbols = "",
-    nums = "1234567890",
-    letters = "qwertyuiopasdfghjklzxcvbnm",
-    upletters = "QWERTYUIOPASDFGHJKLZXCVBNM",
-    symbols = "!$%#&*-+";
+let allsymbols = "",
+    password = "";
+
+const passlength = document.querySelector('#length').value,
+    form = document.querySelector('form');
+
+const passIngridients = {
+    nums: "1234567890",
+    letters: "qwertyuiopasdfghjklzxcvbnm",
+    upletters: "QWERTYUIOPASDFGHJKLZXCVBNM",
+    symbols: "!$%#&*-+"
+};
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    document.querySelectorAll('input[type=checkbox]').forEach(function (elem) {
+
+
+        if (elem.checked) {
+            Object.keys(passIngridients).forEach(function (key) {
+
+                if (key == elem.name) {
+
+                    console.log(`Добавляем...${elem.name}`);
+                    allsymbols += passIngridients[key]; 
+                    console.log(allsymbols);
+                }
+            });
+
+        }
+
+        if (allsymbols == "") {
+            console.log('Error');
+        }
+
+    });
+
+    randomPassword();
+
+    response();
+
+});
+
 
 function randomPassword() {
 
-    let password = "";
+    password = "";
 
-    for (i = 0; i <= length; i++) {
+    for (let i = 0; i <= passlength; i++) {
 
         let pickRandomSymbols = function (symbol) {
             return symbol[Math.floor(Math.random() * symbol.length)]
@@ -19,6 +58,13 @@ function randomPassword() {
         password += pickRandomSymbols(allsymbols);
     }
 
+    console.log(password);
     return password;
 }
 
+function response() {
+    let response = document.createElement('span');
+    //response.classList.add('response');
+    response.textContent = password;
+    form.append(response);
+}
